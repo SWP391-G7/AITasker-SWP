690bcc41f2da2725e8e60b719fb6231996575465
QuytFen <phanngocquyet06@gmail.com>
2026-06-15 21:28:53 +0700

    update avatar on client dashboard

--- src/Components/Dashboard/Client/Messages/Header.jsx
+++ src/Components/Dashboard/Client/Messages/Header.jsx
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 import "../../../../pages/DashboardPage/Client/ClientMarketplace.css"
+import { getStoredUser } from '../../../../Services/checkLogin'
+
+const currentUser = getStoredUser();
+const avatarLetter = () => {
+    const currentUser = getStoredUser()
+    const currentUserName = currentUser?.fullName || "@"
+    return (
+      currentUserName.charAt(0).toUpperCase()
+    )
+  }
+  const userAvatar = avatarLetter()
 
 export default class Header extends Component {
   render() {
@@ -12,7 +23,7 @@ export default class Header extends Component {
 
           <div className="messages-header-actions">
             <button>New Message</button>
-            <span className="market-avatar">A</span>
+            <span className="market-avatar">{userAvatar}</span>
           </div>
         </header>
     )
